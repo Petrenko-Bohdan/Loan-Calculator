550f58bbbbd90541179fc4897937775dae17e43a
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-07-15 19:55:43 +0200

    Add LoanFormComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+NgModule
+import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { LoanFormComponent } from './loan-form/loan-form.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+	{path:'', component: LoanFormComponent},
+	{path:'loan-form', component: LoanFormComponent},
+];
