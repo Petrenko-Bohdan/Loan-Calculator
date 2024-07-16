0ee3fdd1608a39cbc0c213c61f0544621c61c6d7
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-07-16 19:33:56 +0200

    add payment-table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { LoanFormComponent } from './loan-form/loan-form.component';
-
+import { PaymentTableComponent } from './payment-table/payment-table.component';
 
 export const routes: Routes = [
 	{path:'', component: LoanFormComponent},
 	{path:'loan-form', component: LoanFormComponent},
+	{path:'payment-table', component: PaymentTableComponent}
 ];
