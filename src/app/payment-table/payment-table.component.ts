6bdbaf62320db9658b421e31655a79468c0f95d5
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-08-08 19:31:18 +0200

    Add checking and calculate for cases when where `loanPeriod` or `monthlyPaymentAmount` are not specified in the request parameters. In these cases, added calls to `calculateLoanPeriod` and `calculateMonthlyPaymentAmount` methods to calculate the missing data.

--- src/app/payment-table/payment-table.component.ts
+++ src/app/payment-table/payment-table.component.ts
@@ -8,7 +8,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ActivatedRoute } from '@angular/router';
-import { LoanCalculatorService}	from '../loan-calculator.service';
+import { LoanCalculatorService } from '../loan-calculator.service';
 
 @Component({
   selector: 'app-payment-table',
@@ -27,20 +27,26 @@ import { LoanCalculatorService}	from '../loan-calculator.service';
 })
 export class PaymentTableComponent implements OnInit {
   @Input() payments: any[] = [];
-	displayedColumns: string[] = ['month', 'capital', 'interests', 'principalInstalment', 'totalInstallment', 'overpayment', 'profit'];
+  displayedColumns: string[] = ['month', 'capital', 'interests', 'principalInstalment', 'totalInstallment', 'overpayment', 'profit'];
   dataSource = new MatTableDataSource<any>([]);
 
-constructor(private route: ActivatedRoute, private loanCalculatorService: LoanCalculatorService) {}
+  constructor(private route: ActivatedRoute, private loanCalculatorService: LoanCalculatorService) {}
 
   ngOnInit(): void {
-		this.route.queryParams.subscribe((params) => {
-			const loanAmount = +params['loanAmount'];
-			const annualInterestRate = +params['annualInterestRate'];
-			const loanPeriod = +params['loanPeriod'];
-			const monthlyPaymentAmount = +params['monthlyPaymentAmount'];
+    this.route.queryParams.subscribe((params) => {
+      const loanAmount = +params['loanAmount'];
+      const annualInterestRate = +params['annualInterestRate'];
+      let loanPeriod = +params['loanPeriod'];
+      let monthlyPaymentAmount = +params['monthlyPaymentAmount'];
 
-			this.payments = this.loanCalculatorService.calculateMonthlyPayment(loanAmount, annualInterestRate, loanPeriod, monthlyPaymentAmount);
-			this.dataSource.data = this.payments;
-		})
-}
+      if (!loanPeriod && monthlyPaymentAmount) {
+        loanPeriod = this.loanCalculatorService.calculateLoanPeriod(loanAmount, annualInterestRate, monthlyPaymentAmount);
+      } else if (!monthlyPaymentAmount && loanPeriod) {
+        monthlyPaymentAmount = this.loanCalculatorService.calculateMonthlyPaymentAmount(loanAmount, annualInterestRate, loanPeriod);
+      }
+
+      this.payments = this.loanCalculatorService.calculateMonthlyPayment(loanAmount, annualInterestRate, loanPeriod, monthlyPaymentAmount);
+      this.dataSource.data = this.payments;
+    });
+  }
 }
