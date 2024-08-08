865ba2b61f9c163484fbff37ea673ac386ad0b1f
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-08-08 19:41:43 +0200

    Update `calculateMonthlyPayment` method:
    -Method accepts four parameters `loanAmount`, `annualInterestRate`, `loanPeriod` and `monthlyPaymentAmount`.
    
    - Changes in calculations: added `capital`, `interests`, `principalInstalment`, `totalInstallment`, `profit` and `overpayment` fields.
    
    add new methods:
     - `calculateLoanPeriod` to calculate the loan term based on the monthly payment.
    
      - `calculateMonthlyPaymentAmount` to calculate the monthly payment amount based on the loan term.
    
       - Obsolete and unnecessary functions and calculations have been removed.

--- src/app/loan-calculator.service.ts
+++ src/app/loan-calculator.service.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Injectable } from '@angular/core';
 import {format} from 'date-fns';
 
@@ -8,44 +9,59 @@ export class LoanCalculatorService {
 
 	constructor() { }
 
-	calculateMonthlyPayment(principal: number, annualRate: number, term: number, extraPayment: number = 0, overpayments:{month:number, amount:number}[]=[] ):any[]{
+	calculateMonthlyPayment(loanAmount: number, annualInterestRate:number, loanPeriod:number, monthlyPaymentAmount:number): any[]{
+		const payments:any=[];
+		const monthlyInterestsRate=annualInterestRate/12/100;
+		const numberOfPayments=loanPeriod;
+		const monthlyPayment=monthlyPaymentAmount;
 
-	const monthlyRate  = annualRate / 100 / 12;
-	const numberOfPayments = term * 12;
-	const monthlyPayment = principal * monthlyRate  / (1-Math.pow(1+monthlyRate, -numberOfPayments));
-	const payments: any[]=[];
 
-	let balance = principal;
-	let totalInterest = 0;
-	let currentDate = new Date();
+let loanCapital=loanAmount;
+let totalInstallment=0;
+let profit=0;
+let overpayment=0;
+let currentDate = new Date();
 
-	for(let i=0; i<numberOfPayments; i++){
-		const interests = balance * monthlyRate;
-		let principalPayment = monthlyPayment - interests;
-		
-		const overpayment = overpayments.find(p=>p.month === i);
-		if(overpayment){
-			principalPayment += overpayment.amount;
-		}
+for(let i=0; i<numberOfPayments; i++){
+	const capital = loanCapital;
+	const interests = loanCapital*monthlyInterestsRate;
+	const principalInstalment = monthlyPayment-interests;
+	
+	totalInstallment = interests+principalInstalment;
+	profit += interests;
+	overpayment += profit-loanAmount;
+	loanCapital -= principalInstalment;
 
-		balance -= principalPayment;
-		totalInterest += interests;
+	const formatDate=format(currentDate, 'MM/yyyy')
 
-		const formattedDate = format(currentDate, 'MM/yyyy');
-
-		payments.push({
-			month: formattedDate,
-			interests: interests,
-			principal: principalPayment,
-			balance: balance>0?balance:0,
-			totalInterest: totalInterest,
-		});
-		currentDate.setMonth(currentDate.getMonth()+1)
-
-		if(balance<=0) break;
+	payments.push({
+		month: formatDate,
+		capital: capital.toFixed(2),
+		interests: interests.toFixed(2),
+		principalInstalment: principalInstalment.toFixed(2),
+		totalInstallment: totalInstallment.toFixed(2),		
+		profit: profit.toFixed(2),
+		overpayment: overpayment.toFixed(2),
+	});
+	currentDate.setMonth(currentDate.getMonth()+1);
+	if (loanCapital<=0){
+		break;
 	}
-	return payments;
-
 }
+		return payments;
+	}
+
+	calculateLoanPeriod(loanAmount: number, annualInterestRate:number, monthlyPaymentAmount:number):number{
+		const monthlyInterestRate = annualInterestRate/12/100;
+		return Math.ceil(
+			Math.log(monthlyPaymentAmount/(monthlyPaymentAmount-loanAmount*monthlyInterestRate))/Math.log(1 + monthlyInterestRate)
+		)
+	}
+
+	calculateMonthlyPaymentAmount(loanAmount: number, annualInterestRate:number, loanPeriod:number):number{
+		const monthlyInterestRate = annualInterestRate/12/100;
+		const numberOfPayments = loanPeriod;
+		return loanAmount*monthlyInterestRate/(1-Math.pow(1+monthlyInterestRate,-numberOfPayments));
+	}
 
 }
