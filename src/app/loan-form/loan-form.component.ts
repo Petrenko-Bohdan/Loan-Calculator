ad488aae7a6a965fc7594ca13952159079bd8a59
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-08-15 10:47:36 +0200

    Removed the validation of minimum monthly payment amount in `LoanFormComponent`.
    - Deleted logic that checked if the monthly payment amount was sufficient to cover interest and returned an error message if it wasn't.
    - Simplified the `calculateLoan` method to only navigate to the payment table without performing the minimum payment validation.
    - Removed the `errorMessage` property and related logic since the error handling for low payment amounts is no longer needed.

--- src/app/loan-form/loan-form.component.ts
+++ src/app/loan-form/loan-form.component.ts
@@ -36,7 +36,7 @@ import { Router } from '@angular/router';
 export class LoanFormComponent implements OnInit {
   loanform!: FormGroup;
   toggleControl = true;
-	errorMessage: string ='';
+  
 
   constructor(private fb: FormBuilder, private router: Router) {}
 
@@ -85,34 +85,23 @@ export class LoanFormComponent implements OnInit {
     this.onToggleChange();
   }
 
- calculateLoan(){
-	if(this.loanform.invalid){
-		this.loanform.markAllAsTouched();
-		return;
-	}
-	const loanAmount = this.loanform.get('loanAmount')?.value;
-	const annualInterestRate = this.loanform.get('annualInterestRate')?.value;
-	const loanPeriod = this.loanform.get('loanPeriod')?.value;
-	const monthlyPaymentAmount = this.loanform.get('monthlyPaymentAmount')?.value;
-	
-	const monthlyInterestRate = annualInterestRate / 12 / 100;
-  const minimumPayment = loanAmount * monthlyInterestRate;
-	const correctMinimumPayment= minimumPayment + 0.01;
+  calculateLoan() {
+    if (this.loanform.invalid){
+      this.loanform.markAllAsTouched();
+      return;
+    }
+    const loanAmount = this.loanform.get('loanAmount')?.value;
+    const annualInterestRate = this.loanform.get('annualInterestRate')?.value;
+    const loanPeriod = this.loanform.get('loanPeriod')?.value;
+    const monthlyPaymentAmount = this.loanform.get('monthlyPaymentAmount')?.value;
 
-	if (monthlyPaymentAmount <= minimumPayment) {
-		this.errorMessage = 'Monthly payment amount is too low to cover the interest. Minimum payment should be ' + correctMinimumPayment.toFixed(2);
-		return;
-	}
-
-	this.errorMessage = '';
-
-	this.router.navigate(['/payment-table'],{
-		queryParams:{
-			loanAmount,
-			annualInterestRate,
-			loanPeriod,
-			monthlyPaymentAmount,
-		}
-	})
- }
+    this.router.navigate(['/payment-table'], {
+      queryParams: {
+        loanAmount,
+        annualInterestRate,
+        loanPeriod,
+        monthlyPaymentAmount,
+      }
+    });
+  }
 }
