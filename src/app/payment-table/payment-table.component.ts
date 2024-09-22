86f89c3ddfc4df4188eb8c7cf6120359adbf62f1
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-09-22 17:07:01 +0200

    Refactor PaymentTableComponent to use observable for payment data
    - Replaced queryParams logic with subscription to LoanCalculatorService's payment$ observable for handling payment data updates.
    - Removed unnecessary recalculation logic for loan parameters from ngOnInit().
    - Updated `updateOverpayment` method with logging for debugging updated payment data and dataSource.
    - Improved readability and structure by refactoring data initialization.
    - Updated service import to reflect new path in `LoanCalculatorService`.

--- src/app/payment-table/payment-table.component.ts
+++ src/app/payment-table/payment-table.component.ts
@@ -8,10 +8,9 @@ import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ActivatedRoute } from '@angular/router';
-import { LoanCalculatorService } from '../loan-calculator.service';
+import { LoanCalculatorService } from '../services/loan-calculator.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'app-payment-table',
@@ -24,43 +23,44 @@ import { CommonModule } from '@angular/common';
     MatTableModule,
     MatCardModule,
     MatSlideToggleModule,
-		FormsModule,
-		CommonModule
+    FormsModule,
+    CommonModule,
   ],
   templateUrl: './payment-table.component.html',
-  styleUrls: ['./payment-table.component.scss']
+  styleUrls: ['./payment-table.component.scss'],
 })
 export class PaymentTableComponent implements OnInit {
   @Input() payments: any[] = [];
-  displayedColumns: string[] = ['month', 'capital', 'interests', 'principalInstalment', 'totalInstallment', 'overpayment', 'profit'];
-  dataSource = new MatTableDataSource<any>([]);
-	errorMessage: string = '';
+  displayedColumns: string[] = [
+    'month',
+    'capital',
+    'interests',
+    'principalInstalment',
+    'totalInstallment',
+    'overpayment',
+    'profit',
+  ];
+  dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
+  errorMessage: string = '';
 
-  constructor(private route: ActivatedRoute, private loanCalculatorService: LoanCalculatorService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private loanCalculatorService: LoanCalculatorService
+  ) {}
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe((params) => {
-      const loanAmount = +params['loanAmount'];
-      const annualInterestRate = +params['annualInterestRate'];
-      let loanPeriod = +params['loanPeriod'];
-      let monthlyPaymentAmount = +params['monthlyPaymentAmount'];
-
-      if (!loanPeriod && monthlyPaymentAmount) {
-        loanPeriod = this.loanCalculatorService.calculateLoanPeriod(loanAmount, annualInterestRate, monthlyPaymentAmount);
-        this.errorMessage = this.loanCalculatorService.errorMessage;
-      } else if (!monthlyPaymentAmount && loanPeriod) {
-        monthlyPaymentAmount = this.loanCalculatorService.calculateMonthlyPaymentAmount(loanAmount, annualInterestRate, loanPeriod);
-      }
-
-      if (!this.errorMessage) {
-        this.payments = this.loanCalculatorService.calculateMonthlyPayment(loanAmount, annualInterestRate, loanPeriod, monthlyPaymentAmount);
-        this.dataSource.data = this.payments;
-      }
+      this.loanCalculatorService.payment$.subscribe((data) => {
+      this.payments = data;
+      this.dataSource.data = this.payments;
     });
+		
+		
   }
 
-	updateOverpayment(element: any): void {
-    this.payments = this.loanCalculatorService.recalculatePaymentsWithOverpayment(this.payments);
+  public updateOverpayment(element: any): void {
+    // this.payments = this.loanCalculatorService.recalculatePaymentsWithOverpayment(this.payments);
     this.dataSource.data = this.payments;
+    console.log('Updated Payments:', this.payments); // Add this line
+    console.log('Updated DataSource:', this.dataSource.data); // Add this line
   }
 }
