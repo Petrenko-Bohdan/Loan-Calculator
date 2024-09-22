b44b81cfbee2af5bcf06a9271fae2e04d2880630
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-09-22 17:01:37 +0200

    Delete RouterModule and NgModule

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
-NgModule
-import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
-import { RouterModule } from '@angular/router';
 import { LoanFormComponent } from './loan-form/loan-form.component';
 import { PaymentTableComponent } from './payment-table/payment-table.component';
 
