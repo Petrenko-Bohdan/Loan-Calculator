d6c3f90b7bf2ecccca512d2f938a9f27bf346908
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-09-25 20:08:10 +0200

    Refactor LoanCalculatorService: added flexibility for loan calculation
    - Extracted `monthlyInterestRate` into a getter for better code reuse
    - Added methods `calculateLoanPeriod` and `calculateMonthlyPaymentAmount` to compute loan period and monthly payment when missing
    - Implemented fallback logic to calculate `loanPeriod` and `monthlyPaymentAmount` when they are not provided
    - Added console logs in the new methods for debugging purposes
    - Cleaned up the `calculateLoan` method by removing commented-out code

--- src/app/services/loan-calculator.service.ts
+++ src/app/services/loan-calculator.service.ts
@@ -14,24 +14,30 @@ export class LoanCalculatorService {
 	private loanPeriod:number|undefined;
 	private monthlyPaymentAmount:number|undefined;
 
-			
+private get monthlyInterestRate():number{
+		return this.annualInterestRate! / 100 / 12;
+	}
+
 
 	calculateLoan(loanAmount:number, annualInterestRate:number, loanPeriod:number, monthlyPaymentAmount:number):void{
 
 		this.loanAmount = loanAmount;
 		this.annualInterestRate = annualInterestRate;
-		const monthlyInterestRate = this.annualInterestRate! / 100 / 12;
-		
-		this.loanPeriod = loanPeriod;
-		this.monthlyPaymentAmount =  (loanAmount * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -this.loanPeriod));
+				
+		this.loanPeriod = loanPeriod ?? this.calculateLoanPeriod();
+		this.monthlyPaymentAmount = monthlyPaymentAmount ?? this.calculateMonthlyPaymentAmount();
+		// this.monthlyPaymentAmount =  (loanAmount * this.monthlyInterestRate) / (1 - Math.pow(1 + this.monthlyInterestRate, -this.loanPeriod));
+		console.log(this.monthlyPaymentAmount);
+	
+
 		
 		const payments = [];
 		let balance = this.recalculateBalance(loanAmount);
 		let currentDate = new Date();
 		
 		for(let i=0; i<this.loanPeriod; i++){
-			const interest = balance * monthlyInterestRate;			
-			const payment = (loanAmount * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -this.loanPeriod));
+			const interest = balance * this.monthlyInterestRate;			
+			const payment = (loanAmount * this.monthlyInterestRate) / (1 - Math.pow(1 + this.monthlyInterestRate, -this.loanPeriod));
 			const principalInstalment = payment - interest;
 			const formatDate=format(currentDate, 'MM/yyyy')
 
@@ -49,15 +55,24 @@ export class LoanCalculatorService {
 		this.paymentSubject.next(payments);
 	}
 
+	private calculateLoanPeriod():number{		
+		return Math.log(this.monthlyPaymentAmount! / (this.monthlyPaymentAmount! - this.loanAmount! * this.monthlyInterestRate)) / Math.log(1 + this.monthlyInterestRate);
+	console.log("a");
+	}
+
+	private calculateMonthlyPaymentAmount():number{
+		return this.loanAmount! * this.monthlyInterestRate / (1 - Math.pow(1 + this.monthlyInterestRate, -this.loanPeriod!));
+		console.log("b");
+	}
 
 	public recalculateLoan(index:number):void{
 		const payments = this.paymentSubject.getValue();
 		let balance = payments[index].capital- payments[index].principalPayment- payments[index].overpayment;
 
-		const monthlyInterestRate = this.annualInterestRate! / 100 / 12;
+		
 
 		for(let i=index+1; i<payments.length; i++){
-			const interest = balance * monthlyInterestRate; 
+			const interest = balance * this.monthlyInterestRate; 
 			let principal= payments[i].monthlyPayment - interest;
 			const extraPayment = payments[i].overpayment;
 			const payment = principal + interest;
