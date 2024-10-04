6d9cf89388aff5589897a431296c45e0fa84b7c8
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-10-04 17:20:34 +0200

    Application update

--- src/app/loan-form/loan-form.component.ts
+++ src/app/loan-form/loan-form.component.ts
@@ -1,105 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
-import { MatCardModule } from '@angular/material/card';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { CommonModule } from '@angular/common';
-import {
-  AbstractControl,
-  FormBuilder,
-  FormGroup,
-  Validators,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { Router } from '@angular/router';
-import { LoanCalculatorService } from '../services/loan-calculator.service';
+import { Component} from '@angular/core';
+
 
 
 @Component({
   selector: 'app-loan-form',
   standalone: true,
   imports: [
-    MatInputModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatTableModule,
-    MatCardModule,
-    MatSlideToggleModule,
-    CommonModule,
-    ReactiveFormsModule,
-		
+    
   ],
   templateUrl: './loan-form.component.html',
   styleUrls: ['./loan-form.component.scss'],
 })
-export class LoanFormComponent implements OnInit {
-  loanform!: FormGroup;
-  toggleControl = true;
+export class LoanFormComponent  {
+
+
   
 
-  constructor(private fb: FormBuilder, private router: Router, private loanCalculatorService: LoanCalculatorService) {}
 
-  ngOnInit(): void {
-    this.loanform = this.fb.group({
-      loanAmount: ['', [Validators.required, this.decimalPointValidator]],
-      annualInterestRate: [
-        '',
-        [Validators.required, this.decimalPointValidator],
-      ],
-      loanPeriod: [{ value: '', disabled: true }],
-      monthlyPaymentAmount: [
-        { value: '', disabled: true },
-        [this.decimalPointValidator],
-      ],
-    });
+  
 
-    this.onToggleChange();
-  }
 
-  private decimalPointValidator(
-    control: AbstractControl
-  ): { [key: string]: string } | null {
-    const value = control.value;
 
-    if (value === null || value === '') return null;
+  
 
-    const regex = /^\d+([.,]\d{0,2})?$/;
-    return regex.test(value)
-      ? null
-      : { invalidDecimal: 'Invalid decimal value' };
-  }
-
-  public onToggleChange(): void {
-    if (this.toggleControl) {
-      this.loanform.get('loanPeriod')?.enable();
-      this.loanform.get('monthlyPaymentAmount')?.disable();
-    } else {
-      this.loanform.get('loanPeriod')?.disable();
-      this.loanform.get('monthlyPaymentAmount')?.enable();
-    }
-  }
-
-  public toggleChanged(): void {
-    this.toggleControl = !this.toggleControl;
-    this.onToggleChange();
-  }
-
-  public calculateLoan(): void {
-    if (this.loanform.invalid){
-      this.loanform.markAllAsTouched();
-      return;
-    }
-    const loanAmount = this.loanform.get('loanAmount')?.value;
-    const annualInterestRate = this.loanform.get('annualInterestRate')?.value;
-    const loanPeriod = this.loanform.get('loanPeriod')?.value;
-    const monthlyPaymentAmount = this.loanform.get('monthlyPaymentAmount')?.value;
-
-		this.loanCalculatorService.calculateLoan(loanAmount, annualInterestRate, loanPeriod, monthlyPaymentAmount);
-		
-    this.router.navigate(['/payment-table']);
-  }
+  
 }
--- src/app/payment-table/payment-table.component.ts
+++ src/app/payment-table/payment-table.component.ts
@@ -1,66 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
-import { MatCardModule } from '@angular/material/card';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { ActivatedRoute } from '@angular/router';
-import { LoanCalculatorService } from '../services/loan-calculator.service';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-payment-table',
   standalone: true,
   imports: [
-    MatInputModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatTableModule,
-    MatCardModule,
-    MatSlideToggleModule,
-    FormsModule,
-    CommonModule,
+    
   ],
   templateUrl: './payment-table.component.html',
   styleUrls: ['./payment-table.component.scss'],
 })
-export class PaymentTableComponent implements OnInit {
-  @Input() payments: any[] = [];
-  displayedColumns: string[] = [
-    'month',
-    'capital',
-    'interests',
-    'principalInstalment',
-    'totalInstallment',
-    'overpayment',
-    'profit',
-  ];
-  dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
-  errorMessage: string = '';
+export class PaymentTableComponent {
 
-  constructor(
-    private route: ActivatedRoute,
-    private loanCalculatorService: LoanCalculatorService
-  ) {}
-
-  ngOnInit(): void {
-      this.loanCalculatorService.payment$.subscribe((data) => {
-      this.payments = data;
-      this.dataSource.data = this.payments;
-    });
-		
-		
-  }
-
-  public updateOverpayment(element: any): void {
-    // this.payments = this.loanCalculatorService.recalculatePaymentsWithOverpayment(this.payments);
-    this.dataSource.data = this.payments;
-    console.log('Updated Payments:', this.payments); // Add this line
-    console.log('Updated DataSource:', this.dataSource.data); // Add this line
-  }
 }
