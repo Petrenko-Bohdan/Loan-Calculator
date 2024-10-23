d055298270ea8159fd6f252d70556b8e7008bffb
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-10-23 19:45:50 +0200

    Implemented Input to transfer payment data and display a table with columns for month, capital, interest, principal payment, total payment, overpayment and profit. Added ActivatedRoute and LoanCalculatorService dependencies.

--- src/app/payment-table/payment-table.component.ts
+++ src/app/payment-table/payment-table.component.ts
@@ -1,15 +1,27 @@
-import { Component} from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
+import { LoanCalculatorService } from '../services/loan-calculator.service';
+
+
 
 
 @Component({
   selector: 'app-payment-table',
   standalone: true,
   imports: [
-    
+    MatCardModule,
+		MatTableModule
   ],
   templateUrl: './payment-table.component.html',
   styleUrls: ['./payment-table.component.scss'],
 })
 export class PaymentTableComponent {
+	@Input() payments: any[]= [];
+	displayedColumns: string[] = ['month', 'capital', 'interest', 'principalInstalment', 'totalInstalment', 'overpayment', 'profit'];
 
+	constructor(private route: ActivatedRoute, private loanCalculateService: LoanCalculatorService) {}
+
+	
 }
