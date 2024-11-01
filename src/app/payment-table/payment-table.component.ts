eb52342a803dd83e2551f5a9e4e539acfb17edfc
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-11-01 14:31:25 +0100

    Refactor PaymentTableComponent: - Added FormsModule for data binding support. - Integrated MatTableDataSource for improved data handling in table. - Added ngOnInit with subscription to payments$ to dynamically update table data. - Introduced updateOverpayment method for handling overpayment updates.

--- src/app/payment-table/payment-table.component.ts
+++ src/app/payment-table/payment-table.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input} from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
 import { LoanCalculatorService } from '../services/loan-calculator.service';
-
+import { FormsModule } from '@angular/forms';
+import { OnInit } from '@angular/core';
 
 
 
@@ -12,16 +13,26 @@ import { LoanCalculatorService } from '../services/loan-calculator.service';
   standalone: true,
   imports: [
     MatCardModule,
-		MatTableModule
+		MatTableModule,
+		FormsModule,
   ],
   templateUrl: './payment-table.component.html',
   styleUrls: ['./payment-table.component.scss'],
 })
-export class PaymentTableComponent {
+export class PaymentTableComponent implements OnInit {
 	@Input() payments: any[]= [];
 	displayedColumns: string[] = ['month', 'capital', 'interest', 'principalInstalment', 'totalInstalment', 'overpayment', 'profit'];
+	dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
 
 	constructor(private route: ActivatedRoute, private loanCalculateService: LoanCalculatorService) {}
 
-	
+	ngOnInit(): void {
+		this.loanCalculateService.payments$.subscribe((data)=>{
+			this.dataSource.data = this.payments;
+			this.payments = data;	
+		})
+	}
+
+	public updateOverpayment(index: number): void {
+	}
 }
