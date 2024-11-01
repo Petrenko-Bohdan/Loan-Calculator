5d38e572c101e7dd56b84ba3578bb260cd355596
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-11-01 14:32:18 +0100

    Enhance LoanCalculatorService with loan calculation logic: - Added properties for interest rate, loan term, and monthly payment amount. - Implemented methods to calculate monthly interest rate and monthly payment. - Updated calculateLoan to generate an amortization schedule with monthly breakdown. - Integrated date-fns for date formatting in payment schedule.

--- src/app/services/loan-calculator.service.ts
+++ src/app/services/loan-calculator.service.ts
@@ -1,19 +1,65 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-
+import { format } from 'date-fns';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoanCalculatorService {
-	private paymentSubject = new BehaviorSubject<any[]>([]);
-	payments$ = this.paymentSubject.asObservable();
-	private loanAmount!: number;
-	
-	
+  private paymentSubject = new BehaviorSubject<any[]>([]);
+  payments$ = this.paymentSubject.asObservable();
+  private loanAmount!: number;
+  private interestRate!: number;
+  private loanTerm!: number;
+  private monthlyPaymentAmount!: number;
 
-	calculateLoan(loanAmount: number, interestRate: number, loanTerm: number): void {
-		console.log(loanAmount);
-	}
+  private get monthlyInterestRate(): number {
+    return this.interestRate / 12 / 100;
+  }
 
+  private calculateMonthlyPaymentAmount(): number {
+    return (
+      (this.loanAmount! * this.monthlyInterestRate) /
+      (1 - Math.pow(1 + this.monthlyInterestRate, -this.loanTerm!))
+    );
+  }
+
+  public calculateLoan(
+    loanAmount: number,
+    interestRate: number,
+    loanTerm: number
+  ): void {
+    this.loanAmount = loanAmount;
+    this.interestRate = interestRate;
+    this.loanTerm = loanTerm;
+    this.monthlyPaymentAmount = this.calculateMonthlyPaymentAmount();
+
+    const payments = [];
+    let balance = this.loanAmount;
+    let currentDate = new Date();
+
+    for (let i = 0; i < this.loanTerm; i++) {
+      const formatDate = format(currentDate, 'MM/yyyy');
+      const monthlyPayment = this.monthlyPaymentAmount;
+      const interest = this.loanAmount * this.monthlyInterestRate;
+      const principalInstalment = monthlyPayment - interest;
+
+      this.loanAmount -= principalInstalment;
+
+      payments.push({
+        month: formatDate,
+        capital: balance.toFixed(2),
+        interest: interest.toFixed(2),
+        principalInstalment: principalInstalment.toFixed(2),
+        totalInstalment: monthlyPayment.toFixed(2),
+        overpayment: '',
+      });
+
+      currentDate.setMonth(currentDate.getMonth() + 1);
+      balance -= principalInstalment;
+
+      console.log(payments);
+    }
+    this.paymentSubject.next(payments);
+  }
 }
