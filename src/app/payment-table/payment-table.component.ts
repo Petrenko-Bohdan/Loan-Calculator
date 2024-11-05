8a325ab09d514727c9fb7b451d88a74d6df005ba
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-11-05 19:15:40 +0100

    Removed @Input() and payments Property,
    The dataSource.data is directly assigned with the data from the LoanCalculatorService in the ngOnInit method.

--- src/app/payment-table/payment-table.component.ts
+++ src/app/payment-table/payment-table.component.ts
@@ -17,7 +17,6 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./payment-table.component.scss'],
 })
 export class PaymentTableComponent implements OnInit {
-	@Input() payments: any[]= [];
   displayedColumns: string[] = ['month', 'capital', 'interest', 'principalInstalment', 'overpayment', 'profit',];
   dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
 
@@ -25,8 +24,7 @@ export class PaymentTableComponent implements OnInit {
 
   ngOnInit(): void {
     this.loanCalculateService.payments$.subscribe((data)=>{
-      this.payments = data;
-      this.dataSource.data = this.payments;
+      this.dataSource.data = data;
     });
   }
 
