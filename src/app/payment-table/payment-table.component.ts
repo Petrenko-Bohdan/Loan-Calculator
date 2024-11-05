727741c3a8f0f7503e22d7214ae20f23596fe5b1
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-11-05 20:18:36 +0100

    Added OnInit interface to PaymentTableComponent for proper ngOnInit lifecycle support
    Implement unsubscribe$ in PaymentTableComponent to prevent memory leaks.
    Add updateOverpayment method to recalculate the payment schedule when extra payments are made.

--- src/app/payment-table/payment-table.component.ts
+++ src/app/payment-table/payment-table.component.ts
@@ -1,9 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
 import { LoanCalculatorService } from '../services/loan-calculator.service';
 import { FormsModule } from '@angular/forms';
+import { takeUntil } from 'rxjs';
+import { Subject } from 'rxjs';
+
 
 @Component({
   selector: 'app-payment-table',
@@ -16,17 +19,23 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './payment-table.component.html',
   styleUrls: ['./payment-table.component.scss'],
 })
-export class PaymentTableComponent implements OnInit {
+export class PaymentTableComponent implements OnDestroy, OnInit {
   displayedColumns: string[] = ['month', 'capital', 'interest', 'principalInstalment', 'overpayment', 'profit',];
   dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
+	private unsubscribe$ = new Subject<void>();
 
   constructor(private route: ActivatedRoute, private loanCalculateService: LoanCalculatorService) {}
 
   ngOnInit(): void {
-    this.loanCalculateService.payments$.subscribe((data)=>{
+    this.loanCalculateService.payments$.pipe(takeUntil(this.unsubscribe$)).subscribe((data)=>{
       this.dataSource.data = data;
     });
   }
+
+	ngOnDestroy(): void {
+		this.unsubscribe$.next();
+		this.unsubscribe$.complete();
+	}
 
   public updateOverpayment(index: number): void {
     this.loanCalculateService.calculateLoanWithOverpayment(index);
