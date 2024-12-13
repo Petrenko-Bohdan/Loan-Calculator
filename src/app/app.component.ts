1bee0c5c435468d17279839e13c2567c8a58a6d6
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-12-13 19:06:01 +0100

    - Added LanguageSwitcherComponent to the imports of AppComponent. - Prepared the main application component to include the language switching functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoanFormComponent } from './loan-form/loan-form.component';
+import { LanguageSwitcherComponent } from './language-switcher/language-switcher.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoanFormComponent],
+  imports: [RouterOutlet, LoanFormComponent, LanguageSwitcherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
