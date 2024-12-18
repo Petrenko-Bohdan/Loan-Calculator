a9744de4f444a7116982f8bd6c8e431b34006ebc
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-12-18 19:22:09 +0100

    - Integrated dynamic translations for table columns like `month`, `capital`, `interest`, `principalInstalment`, `overpayment`, and `profit`. - Subscribed to language changes from `LanguageService` to reflect the current language in the table. - Updated `PaymentTableComponent` to handle language-specific content rendering.

--- src/app/payment-table/payment-table.component.ts
+++ src/app/payment-table/payment-table.component.ts
@@ -7,6 +7,9 @@ import { FormsModule } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { PaymentElement } from '../models/payment-element.model';
+import { translations } from '../shared/translations';
+import { LanguageService } from '../services/language.service';
+import { Translations } from '../models/translations.model';
 
 
 @Component({
@@ -24,12 +27,18 @@ import { PaymentElement } from '../models/payment-element.model';
 export class PaymentTableComponent implements OnInit {
   displayedColumns: string[] = ['month', 'capital', 'interest', 'principalInstalment', 'overpayment', 'profit',];
   payments$!: Observable<any>;
+	currentLanguage: keyof Translations = 'en';
+  translations: Translations = translations;
 	
 
-  constructor(private route: ActivatedRoute, private loanCalculateService: LoanCalculatorService) {}
+  constructor(private route: ActivatedRoute, private loanCalculateService: LoanCalculatorService, private languageService: LanguageService) {}
 
   ngOnInit(): void {
     this.payments$ = this.loanCalculateService.payments$;
+
+		this.languageService.language$.subscribe((language) => {
+      this.currentLanguage = language as keyof Translations;
+    });
   }
 
 	getOverpayment(element: PaymentElement): number | null {
