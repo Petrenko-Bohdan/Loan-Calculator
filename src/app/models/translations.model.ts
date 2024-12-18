f5cd8f9f08e328397d7316109e379e394f8c3dad
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-12-18 19:20:26 +0100

    - Defined a `Translations` interface to strongly type translation objects. - Included keys for loan-related terms such as `loanAmount`, `interestRate`, `calculate`, etc. - Ensured compatibility with the dynamic multilingual feature.

---
+++ src/app/models/translations.model.ts
@@ -0,0 +1,15 @@
+export interface Translations {
+  [key: string]: {
+    loanAmount: string;
+    interestRate: string;
+    loanTerm: string;
+    monthlyPayment: string;
+    month: string;
+    capital: string;
+    interest: string;
+    principalInstalment: string;
+    overpayment: string;
+    profit: string;
+    calculate: string;
+  };
+}
